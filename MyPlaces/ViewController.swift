f59c5464b3a40cc1e25f0149b59b6503eb98b340
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-01-13 14:44:45 -0800

    Add the annotation code for selected place

--- MyPlaces/ViewController.swift
+++ MyPlaces/ViewController.swift
@@ -17,7 +17,51 @@ class ViewController: UIViewController, MKMapViewDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print(activePlace)
+        if activePlace != -1 {
+            
+            // Get details for chosen place to display on the map
+            
+            if places.count > activePlace {
+               
+                if let name = places[activePlace]["name"] {
+                    
+                    if let lat = places[activePlace]["lat"] {
+                        
+                        if let lon = places[activePlace]["lon"] {
+                            
+                            let latitude: CLLocationDegrees = Double(lat)!
+                            
+                            let longitude: CLLocationDegrees = Double(lon)!
+                            
+                            let latDelta: CLLocationDegrees = 0.03
+                            
+                            let lonDelta: CLLocationDegrees = 0.03
+                            
+                            let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+                            
+                            let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+                            
+                            let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+                            
+                            map.setRegion(region, animated: true)
+                            
+                            let annotation = MKPointAnnotation()
+                            
+                            annotation.title = name
+                            
+                            annotation.coordinate = location
+                            
+                            map.addAnnotation(annotation)
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
