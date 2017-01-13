17a29420bf3a3394440caf4b706d9ec48059d181
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-01-13 15:39:06 -0800

    Add delete functionality to the table view

--- MyPlaces/PlacesViewController.swift
+++ MyPlaces/PlacesViewController.swift
@@ -83,17 +83,20 @@ class PlacesViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
         if editingStyle == .delete {
             // Delete the row from the data source
+            places.remove(at: indexPath.row)
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            
+            
+        }
     }
-    */
+
 
     /*
     // Override to support rearranging the table view.
